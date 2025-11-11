a0f54707c913f048f8878e9b92480f1ceea48d11
DorianLeci <dleci00@fesb.hr>
2025-11-11 15:43:40 +0100

    Funcionality 4-"User overwiew made" + all variable and function names changed to english

--- Internship-2-C-Sharp-App/Program.cs
+++ Internship-2-C-Sharp-App/Program.cs
@@ -1,48 +1,53 @@
-﻿namespace Internship_2_C_Sharp_App;
+﻿using System.Globalization;
 
-using Korisnik = Dictionary<int, Tuple<string, string,
+namespace Internship_2_C_Sharp_App;
+
+using User = Dictionary<int, Tuple<string, string,
     DateOnly, Dictionary<int, Tuple<DateOnly, double, double, double, double>>>>;
 
-using Putovanje=Dictionary<int, Tuple<DateOnly, double, double, double, double>>;
+using Trip=Dictionary<int, Tuple<DateOnly, double, double, double, double>>;
+
+using UserDictValue=Tuple<string, string,
+    DateOnly, Dictionary<int, Tuple<DateOnly, double, double, double, double>>>;
 class Program
 {
     static void Main(string[] args)
     {
-        var korisnik = new Korisnik();
+        var user = new User();
 
-        var korisnik1PopisPutovanja = new Putovanje()
+        var user1TripList = new Trip()
         {
             {1,Tuple.Create(new DateOnly(2025, 10, 10), 220.0, 30.0, 1.49, 44.7)},
             {2,Tuple.Create(new DateOnly(2025, 11, 10), 235.0, 35.0, 1.49, 52.15)}
         };
 
-        var korisnik2PopisPutovanja = new Putovanje()
+        var user2TripList = new Trip()
         {
             {3,Tuple.Create(new DateOnly(2023, 03, 15), 220.0, 30.0, 1.49, 44.7)},
             {4,Tuple.Create(new DateOnly(2023, 04, 16), 235.0, 35.0, 1.49, 52.15)},
             {5,Tuple.Create(new DateOnly(2023, 02, 17), 50.0, 10.0, 1.20, 12.0)}
         };
 
-        var korisnik3PopisPutovanja = new Putovanje()
+        var user3TripList = new Trip()
         {
             { 6, Tuple.Create(new DateOnly(2024, 03, 15), 100.0, 40.0, 1.20, 48.0) },
             { 7, Tuple.Create(new DateOnly(2024, 12, 12), 120.0, 45.0, 1.20, 54.0) },
         };
-        korisnik[1] = Tuple.Create("Dorian", "Leci",new DateOnly(2004,03,28), korisnik1PopisPutovanja);
-        korisnik[2] = Tuple.Create("Vesna", "Leci", new DateOnly(1971, 01, 23), korisnik2PopisPutovanja);
-        korisnik[3]=Tuple.Create("Damir","Leci",new DateOnly(1969,05,18), korisnik3PopisPutovanja);
+        user[1] = Tuple.Create("Dorian", "Leci",new DateOnly(2004,03,28), user1TripList);
+        user[2] = Tuple.Create("Vesna", "Leci", new DateOnly(1971, 01, 23), user2TripList);
+        user[3]=Tuple.Create("Damir","Leci",new DateOnly(1969,05,18), user3TripList);
         
-        GlavniIzbornik(korisnik);
+        MainMenu(user);
     }
 
-    static void GlavniIzbornik(Korisnik dictKorisnika)
+    static void MainMenu(User userDict)
     {
         while (true)
         {
             Console.WriteLine("1 - Korisnici\n2 - Putovanja\n0 - Izlaz iz aplikacije\n");
-            if (int.TryParse(Console.ReadLine(), out int unosGlavniIzbornik ))
+            if (int.TryParse(Console.ReadLine(), out int inputMainMenu))
             {
-                switch (unosGlavniIzbornik)
+                switch (inputMainMenu)
                 {
                     case 0:
                         Console.WriteLine("Uspješan odabir.izlaz iz aplikacije\n");
@@ -50,24 +55,24 @@ class Program
                         break;
                     case 1:
                         Console.WriteLine("Uspješan odabir menija za korisnike.\n");
-                        KorisniciIzbornik(dictKorisnika);
+                        UserMenu(userDict);
                         break;
                     case 2:
                         Console.WriteLine("Uspješan odabir menija za putovanja.\n");
-                        PutovanjeIzbornik(dictKorisnika);
+                        TripMenu(userDict);
                         break;
                     default:
-                        Console.WriteLine("Pogrešan unos broja.Pokušaj ponovno\n");
+                        Console.WriteLine("Unos nije među ponuđenima.Unesi ponovno");
                         break;
                 }
             }
             else
             {
-                Console.WriteLine("Pogrešan format unosa->mora biti integer\n");
+                Console.WriteLine("Pogrešan tip podatka->unesi cijeli broj.");
             }
         }        
     }
-    static void KorisniciIzbornik(Korisnik dictKorisnika)
+    static void UserMenu(User userDict)
     {
         while (true)
         {
@@ -76,17 +81,19 @@ class Program
             Console.WriteLine("3 - Uređivanje korisnika\n");
             Console.WriteLine("4 - Pregled svih korisnika\n");
             Console.WriteLine("0 - Povratak na glavni izbornik\n");
-            if (int.TryParse(Console.ReadLine(), out int unosKorisniciIzbornik))
+            if (int.TryParse(Console.ReadLine(), out int inputUserMenu))
             {
-                switch (unosKorisniciIzbornik)
+                switch (inputUserMenu)
                 {
                     case 0:
                         Console.WriteLine("Uspješan odabir.Povratak na glavni izbornik\n");
-                        GlavniIzbornik(dictKorisnika);
+                        MainMenu(userDict);
                         return;
                     case 1:
                         Console.WriteLine("Uspješan odabir.Unos novog korisnika\n");
-                        UnosNovogKorisnika(dictKorisnika);
+                        NewUserInput(userDict);
+                        Console.WriteLine("...Čeka se any key od korisnika...");
+                        Console.Read();
                         break;
                     case 2:
                         Console.WriteLine("Uspješan odabir.Brisanje korisnika\n");
@@ -95,22 +102,24 @@ class Program
                         Console.WriteLine("Uspješan odabir.Uređivanje korisnika\n");
                         break;
                     case 4:
-                        Console.WriteLine("Uspješan odabir.Pregled svih korisnika\n");
-                        AbecedniIspisKorisnika(dictKorisnika);
+                        Console.WriteLine("Uspješan odabir.Pregled svih korisnika");
+                        FormatedOutputType(userDict);
+                        Console.WriteLine("...Čeka se any key od korisnika...");
+                        Console.Read();
                         break;
                     default:
-                        Console.WriteLine("Pogrešan odabir.Ponovno unesi\n");
+                        Console.WriteLine("Unos nije među ponuđenima.Unesi ponovno");
                         break;
                 }
             }
             else
             {
-                Console.WriteLine("Pogrešan format unosa->mora biti integer\n");
+                Console.WriteLine("Pogrešan tip podatka->unesi cijeli broj.");
             }            
         }
     }
 
-    static void PutovanjeIzbornik(Korisnik dictKorisnika)
+    static void TripMenu(User userDict)
     {
         while (true)
         {
@@ -120,13 +129,13 @@ class Program
             Console.WriteLine("4 - Pregled svih putovanja\n");
             Console.WriteLine("5 - Izvještaji i analize\n");
             Console.WriteLine("0 - Povratak na glavni izbornik\n");
-            if (int.TryParse(Console.ReadLine(), out int unosPutovanjeIzbornik))
+            if (int.TryParse(Console.ReadLine(), out int inputTripMenu))
             {
-                switch (unosPutovanjeIzbornik)
+                switch (inputTripMenu)
                 {
                     case 0:
                         Console.WriteLine("Uspješan odabir.Povratak na glavni izbornik\n");
-                        GlavniIzbornik(dictKorisnika);
+                        MainMenu(userDict);
                         return;
                     case 1:
                         Console.WriteLine("Uspješan odabir.Unos novog putovanja\n");
@@ -144,75 +153,153 @@ class Program
                         Console.WriteLine("Uspješan odabir.Izvještaji i analize\n");
                         break;
                     default:
-                        Console.WriteLine("Pogrešan odabir.Ponovno unesi\n");
+                        Console.WriteLine("Unos nije među ponuđenima.Unesi ponovno.");
                         break;
                 }
             }
             else
             {
-                Console.WriteLine("Pogrešan format unosa->mora biti integer\n");
+                Console.WriteLine("Pogrešan tip podatka->unesi cijeli broj.");
             }   
         }        
     }
 
-    static void UnosNovogKorisnika(Korisnik dictKorisnika)
+    static void NewUserInput(User userDict)
     {
-        int id = UnesiId(dictKorisnika);
-        string ime = UnesiString("ime");
-        string prezime= UnesiString("prezime");
-        DateOnly datum = UnesiDatumRodenja();
-        dictKorisnika[id] = Tuple.Create(ime, prezime, datum,new Putovanje());
+        int id = IdInput(userDict);
+        string name = StringInput("ime");
+        string surname= StringInput("prezime");
+        DateOnly birthDate = BirthDateInput();
+        userDict[id] = Tuple.Create(name, surname,birthDate,new Trip());
         
     }
 
-    static int UnesiId(Korisnik dictKorisnika)
+    static int IdInput(User userDict)
     {
         while (true)
         {
             Console.WriteLine("Unesi jedinstveni id korisnika");
-            if (int.TryParse(Console.ReadLine(), out int unosId) && unosId>=0 && !dictKorisnika.ContainsKey(unosId))
-                return unosId;
+            if (int.TryParse(Console.ReadLine(), out int inputId) && inputId>=0 && !userDict.ContainsKey(inputId))
+                return inputId;
             
-            if(unosId<0)
+            if(inputId<0)
                 Console.WriteLine("Id ne smije biti negativan broj.");
-            else if(dictKorisnika.ContainsKey(unosId))
+            else if(userDict.ContainsKey(inputId))
                 Console.WriteLine("Id mora biti jedinstven.");
+            else
+            {
+                Console.WriteLine("Pogrešan tip podatka-> id mora biti cijeli broj");
+            }
         }
     }
 
-    static string UnesiString(string poljeUnosa)
+    static string StringInput(string nameSurnameOutput)
     {
         while (true)
         {
-            Console.WriteLine("Unesi {0} korisnika",poljeUnosa);
-            string? unosIme = Console.ReadLine();
-            if (!string.IsNullOrEmpty(unosIme) && unosIme.All(stringChar => char.IsLetter(stringChar)))
+            Console.WriteLine("Unesi {0} korisnika",nameSurnameOutput);
+            string? inputNameSurname = Console.ReadLine();
+            if (!string.IsNullOrEmpty(inputNameSurname) && inputNameSurname.All(stringChar => char.IsLetter(stringChar)))
             {
-                string unosImeUpper=char.ToUpper(unosIme[0])+unosIme.Substring(1).ToLower();
-                return unosImeUpper;
+                string inputNameSurnameUpper=char.ToUpper(inputNameSurname[0])+inputNameSurname.Substring(1).ToLower();
+                return inputNameSurnameUpper;
             }
 
-            Console.WriteLine("Pogrešan unos {0}na .Ne smije biti prazno ili sadržavati brojeve/specijalne znakove",poljeUnosa);
+            Console.WriteLine("Pogrešan unos {0}na .Ne smije biti prazno ili sadržavati brojeve/specijalne znakove",nameSurnameOutput);
         }       
     }
 
-    static DateOnly UnesiDatumRodenja()
+    static DateOnly BirthDateInput()
     {
         while (true)
         {
             Console.WriteLine("Unesi datum rođenja (YYYY-MM-DD)");
-            if (DateOnly.TryParse(Console.ReadLine(), out DateOnly unosDatumRodenja))
-                return unosDatumRodenja;
+            if (DateOnly.TryParse(Console.ReadLine(), out DateOnly inputBirthDate))
+                return inputBirthDate;
             Console.WriteLine("Pogrešan unos datuma rođenja.");
         }               
     }
 
-    static void AbecedniIspisKorisnika(Korisnik dictKorisnika)
+    static void AlphabetSortedOutput(User userDict)
     {
-        var dictSorted=dictKorisnika.OrderBy(keyValuePair=>keyValuePair.Value.Item2);
-        foreach (var keyValuePair in dictSorted)
+        var dictSorted=userDict.OrderBy(kvPar=>kvPar.Value.Item2);
+        foreach (var kvPair in dictSorted)
         {
-            Console.WriteLine("{0} - {1} - {2} - {3}\n",keyValuePair.Key,keyValuePair.Value.Item1,keyValuePair.Value.Item2,keyValuePair.Value.Item3);
+            FormatedOutput(kvPair);
         }
+    }
+
+    static void YearTresholdOutput(User userDict)
+    {
+        Console.WriteLine("Ispis korisnika starijih od 20 godina\n");
+        foreach (var user in userDict)
+        {
+  
+            var birthDate = user.Value.Item3;
+            var birthDateTime = birthDate.ToDateTime(new TimeOnly()).Date;
+            var newestDate = DateTime.Today;
+            int age = newestDate.Year-birthDate.Year;
+            
+            if (birthDateTime> newestDate.AddYears(-age))
+                age--;
+            
+            if (age > 20)
+                FormatedOutput(user);         
+        }
+    }
+
+    static void FormatedOutputType(User userDict)
+    {
+        while (true)
+        {
+            Console.WriteLine("0- povratak na KorisnickiIzbornik");
+            Console.WriteLine("1- Ispis korisnika sortiranih abecedno po prezimenu");
+            Console.WriteLine("2 - Ispis korisnika starijih od 20 godina"); 
+            Console.WriteLine("3- Ispis korisnika koji imaju bar 2 putovanja");
+            if (int.TryParse(Console.ReadLine(),out int inputNumber))
+            {
+                switch (inputNumber)
+                {
+                    case 0:
+                        Console.WriteLine("Uspješan odabir.Povratak na glavni izbornik\n");
+                        UserMenu(userDict);
+                        return;
+                    case 1:
+                        AlphabetSortedOutput(userDict);
+                        Console.WriteLine("...Čeka se any key od korisnika...");
+                        Console.Read();
+                        break;
+                    case 2:
+                        YearTresholdOutput(userDict);
+                        Console.WriteLine("...Čeka se any key od korisnika...");
+                        Console.Read();
+                        break;
+                    case 3:
+                        TripTrehsoldOutput(userDict);
+                        Console.WriteLine("...Čeka se any key od korisnika...");
+                        Console.Read();
+                        break;
+                    default:
+                        Console.WriteLine("Unos nije među ponuđenima.Unesi ponovno.");
+                        break;
+                }
+            }   
+            else Console.WriteLine("Pogrešan tip podatka->unesi cijeli broj.");
+        }
+        
+    }
+
+    static void TripTrehsoldOutput(User userDict)
+    {
+        foreach (var user in userDict )
+        {
+            if (user.Value.Item4.Count() >=2)
+                FormatedOutput(user);
+        }   
+    }
+
+    static void FormatedOutput(KeyValuePair<int,UserDictValue> dictElement)
+    {
+        Console.WriteLine("{0} - {1} - {2} - {3}\n",dictElement.Key,dictElement.Value.Item1,dictElement.Value.Item2,dictElement.Value.Item3);           
     }
 }
